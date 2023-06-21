b50b9116cce319543178f12e95bc121a68301958
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-06-21 15:35:33 -0300

    se realizaron cambios en app.js, se agregaron middlewares y manejo de errores 404

--- server/app.js
+++ server/app.js
@@ -8,6 +8,10 @@ const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -15,6 +19,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
-
+app.use((req, res, next) => {
+  res.write(`<div>
+        <h1>404 - Ruta no encontrada</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Redireccionando a la página de inicio...</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/tareas';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`);
+});
 // Starting the server
-app.listen(45635, () => console.log("Server on port xxxx"));
+app.listen(45635, () => console.log("Server on port 4000"));
