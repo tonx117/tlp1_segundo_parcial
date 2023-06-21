7dd46406510636bd85c744b94968d3f85b2bb666
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-06-21 15:57:36 -0300

    realizacion de tabla modelos en reserva.js

--- server/models/Reserva.js
+++ server/models/Reserva.js
@@ -1,1 +1,32 @@
 // TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require("../db");
+
+const Reserva = sequelize.define(
+  "Reserva",
+  {
+    cliente: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    codreserva: {
+      type: DataTypes.INET,
+      allowNull: false,
+    },
+    fechareserva: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+  },
+  {
+    // Other model options go here
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: "Reserva",
+  }
+);
+// Crear tabla si no existe
+Reserva.sync();
+
+module.exports = Reserva;
